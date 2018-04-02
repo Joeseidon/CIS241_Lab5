ee534aced70b64e656e453efa391908d3c2a77af
Joseph <cutinoj@mail.gvsu.edu>
2018-04-02 14:33:00 -0400

    initial

---
+++ test.sh
@@ -0,0 +1,25 @@
+#1/bin/bash
+
+if $# = 0
+	then 
+		echo "A command line argument is required"
+		exit 1
+fi
+
+for f in "$@"; do
+	echo "$f"
+	if test "$f" = "-1"
+		then
+			echo "L entered"
+	elif test"$f" = "-c"
+		then
+			echo "C entered"
+	elif test "$f" = "--help"
+		then 
+			echo "help entered"
+	fi
+done
+
+[ -d ~/backup] && echo "Directory Exists" || mkdir ~/backup
+
+ls -l
