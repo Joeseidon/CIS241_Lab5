813d53926788a787eb793aa8377844f4a239e119
Joseph <cutinoj@mail.gvsu.edu>
2018-04-02 22:30:19 -0400

    test

--- test.sh
+++ test.sh
@@ -1,35 +1,43 @@
 #1/bin/bash
 
+#Determine if there is a command line argument
 if [ $# -eq 0 ]
 	then 
 		echo "A command line argument is required"
 		exit 1
 fi
 
+#Determine if there are too many command line arguments 
 if [[ $# > 1 ]]
 	then
 		echo "Only one argument"
 		exit 1
 fi
 
+#Determine action for command line argument
 for f in "$@"; do
 	echo "$f"
 	if [[ "$f" = "-1" ]]
 		then
-			echo "L entered"
+			echo "Local Files and Directories:"
 			ls -las --human-readable
 	elif [[ "$f" = "-c" ]]
 		then
-			echo "C entered"
+			echo "File and Directory Count:"
+			#Locates all files (including hidden files) in the current directory and sub directories
+			find .//. ! -name . -print | grep -c //
+			echo "Memory Usage:"
+			#Display the number of bytes used by files and directories in the current directory 
+			find . -type f -name . -exec du -ch {} + | grep total$
 	elif [[ "$f" = "--help" ]]
 		then 
-			echo "help entered"
+			echo "To run this file, type sh followed by test.sh (this program)."
+			echo "Before pressing enter, you must enter one of the following arguments:"
+			echo "-l, to display all files"
+			echo "-c, to count the files and display bytes used"
 	fi
 done
 
-find . ! -name . -prune -print | grep -c /
 
-
+#Creates a backup folder in the home directory if it doesn't already exist 
 [ -d ~/backup ] && echo "Directory Exists" || mkdir ~/backup
-
-ls -ls --human-readable
